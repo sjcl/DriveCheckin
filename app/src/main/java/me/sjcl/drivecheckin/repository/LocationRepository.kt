f6a4760205666e87fcc042a351893747f8492ca5
sjcl <bebe@sjcl.me>
2024-03-26 17:47:38 +0900

    RequestLocationUpdates

--- app/src/main/java/me/sjcl/drivecheckin/repository/LocationRepository.kt
+++ app/src/main/java/me/sjcl/drivecheckin/repository/LocationRepository.kt
@@ -6,28 +6,48 @@ import android.location.Location
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class LocationRepository @Inject constructor(
-    @ApplicationContext private val context: Context,
-    private val fusedLocationProviderClient: FusedLocationProviderClient
-) {
+    @ApplicationContext private val context: Context) {
+    private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+    private val _location = MutableLiveData<Location>()
+    val location: LiveData<Location> = _location
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            for (location in locationResult.locations) {
+                _location.postValue(location)
+            }
+            stopLocationUpdates()
+        }
+    }
+
     @SuppressLint("MissingPermission")
-    fun getCurrentLocationOnce(): LiveData<Location?> {
-        val currentLocation = MutableLiveData<Location?>()
+    fun startLocationUpdates() {
+        val locationRequest = LocationRequest.Builder(1000)
+            .setWaitForAccurateLocation(true)
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+            .build()
 
-        fusedLocationProviderClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                currentLocation.value = location
-            }
-            .addOnFailureListener {
-                currentLocation.value = null
-            }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
+    }
 
-        return currentLocation
+    fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
 }
--- app/src/main/java/me/sjcl/drivecheckin/ui/home/HomeViewModel.kt
+++ app/src/main/java/me/sjcl/drivecheckin/ui/home/HomeViewModel.kt
@@ -2,7 +2,6 @@ package me.sjcl.drivecheckin.ui.home
 
 import android.location.Location
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import me.sjcl.drivecheckin.repository.LocationRepository
@@ -13,13 +12,9 @@ class HomeViewModel @Inject constructor(
     private val locationRepository: LocationRepository
 ) : ViewModel() {
 
-    private val _location = MutableLiveData<Location?>()
-    val location: LiveData<Location?> = _location
+    val location: LiveData<Location> = locationRepository.location
 
     fun updateLocation() {
-        val currentLocation = locationRepository.getCurrentLocationOnce()
-        currentLocation.observeForever { location ->
-            _location.value = location
-        }
+       locationRepository.startLocationUpdates()
     }
 }
